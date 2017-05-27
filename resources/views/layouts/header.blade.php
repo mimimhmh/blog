b2a181167c2d717e4d6c04ab0dab9b49edfea3a1
Yang Wang <mimimhmh@users.noreply.github.com>
2017-05-27 23:55:31 +1200

    Posts view layout setup

---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,23 @@
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Blog Template for Bootstrap</title>
+
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css"
+          integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+    <script
+            src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+            integrity="sha256-k2WSCIexGzOj3Euiig+TlR8gA0EmPjuc79OEeY5L45g="
+            crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js"
+            integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn"
+            crossorigin="anonymous"></script>
+
+    <!-- Custom styles for this template -->
+    <link href="{{URL::asset('css/blog.css')}}" rel="stylesheet">
+</head>
---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,23 @@
+@extends ('layouts.master')
+
+@section ('content')
+
+    <div class="col-sm-8 blog-main">
+
+        @foreach($posts as $post)
+        <div class="blog-post">
+            <h2 class="blog-post-title">{{ $post->title }}</h2>
+            <p class="blog-post-meta">January 1, 2014 by <a href="#">Mark</a></p>
+        </div><!-- /.blog-post -->
+        @endforeach
+
+
+        <nav class="blog-pagination">
+            <a class="btn btn-outline-primary" href="#">Older</a>
+            <a class="btn btn-outline-secondary disabled" href="#">Newer</a>
+        </nav>
+
+    </div><!-- /.blog-main -->
+
+
+@endsection

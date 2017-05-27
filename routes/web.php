72272ffb3b6ee9b567543b408098218f8bbacf9e
Yang Wang <mimimhmh@users.noreply.github.com>
2017-05-27 21:31:53 +1200

    Add controller

--- routes/web.php
+++ routes/web.php
@@ -1,24 +1,8 @@
 <?php
 
-Use App\Task;
 
-Route::get('/', function ()
-{
-    $name = 'John Doe';
 
-    return view('welcome', compact('name'));
-});
+Route::get('/', 'TasksController@index');
+Route::get('/tasks', 'TasksController@index');
 
-Route::get('/tasks', function ()
-{
-    $tasks = Task::all();
-
-    return view('tasks.index', compact('tasks'));
-});
-
-Route::get('/tasks/{task}', function ($id)
-{
-    $task = Task::find($id);
-
-    return view('tasks.show', compact('task'));
-});
+Route::get('/tasks/{task}', 'TasksController@show');

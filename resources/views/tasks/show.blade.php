0845f17dfce7ead692679c7f5a106ffe8cb56e72
Yang Wang <mimimhmh@users.noreply.github.com>
2017-05-27 18:57:19 +1200

    Mock data.

---
+++ resources/views/tasks/show.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Welcome</title>
+</head>
+
+<body>
+
+  <h1> {{ $task->body }} </h1>
+
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,13 @@ Route::get('/', function () {
     $name = 'John Doe';
     return view('welcome', compact('name'));
 });
+
+Route::get('/tasks', function () {
+    $tasks = DB::table('tasks')->get();
+    return view('tasks.index', compact('tasks'));
+});
+
+Route::get('/tasks/{task}', function ($id) {
+    $task = DB::table('tasks')->find($id);
+    return view('tasks.show', compact('task'));
+});

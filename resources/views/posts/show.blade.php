9ef39d2f1d56cb834b6eef0d0078165529c052ee
Yang Wang <mimimhmh@users.noreply.github.com>
2017-05-30 00:50:08 +1200

    bug fixing

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -18,7 +18,7 @@
                 @foreach($post->comments as $comment)
                     <button type="button" class="list-group-item list-group-item-action">
                         <strong>
-                            {{ auth()->user()->name }}
+                            {{ $comment->user->name }}
                             &nbsp; at
                             {{ $comment->created_at->diffForHumans() }}: &nbsp;&nbsp;
                         </strong>
@@ -27,7 +27,6 @@
                 @endforeach
             </div>
         </div>
-
         @include('posts.comment')
 
         @include('layouts.errors')
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,5 @@
 <?php
 
-dd(resolve('App\Billing\Stripe'));
-
 Route::get('/tasks', 'TasksController@index');
 Route::get('/tasks/{task}', 'TasksController@show');
 

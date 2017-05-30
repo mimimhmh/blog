9fbfe2512fe6cc16b2a8bd55ae32f326c469c3a8
Yang Wang <mimimhmh@users.noreply.github.com>
2017-05-31 02:11:31 +1200

    basic pagination

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -15,7 +15,7 @@ class PostsController extends Controller
 
     public function index() {
 
-        $posts = Post::latest()->filter(request(['month', 'year']))->get();
+        $posts = Post::latest()->filter(request(['month', 'year']))->paginate(5);
 
         return view('posts.index', compact('posts'));
     }
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -8,11 +8,9 @@
             @include('posts.post')
         @endforeach
 
-
-        <nav class="blog-pagination">
-            <a class="btn btn-outline-primary" href="#">Older</a>
-            <a class="btn btn-outline-secondary disabled" href="#">Newer</a>
-        </nav>
+        <ul class="pagination">
+            {{ $posts->links() }}
+        </ul>
 
     </div><!-- /.blog-main -->
 
